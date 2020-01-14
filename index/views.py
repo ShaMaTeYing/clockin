1569da6bd67288347b052018cb4bd68a36d95e5a
WuYing <529194793@qq.com>
2020-01-14 17:25:55 +0800

    更新python3.5对于json的小bug

--- index/views.py
+++ index/views.py
@@ -6,5 +6,5 @@ from .autocheck import CheckIn
 def index(request):
     autocheck = CheckIn()
     res = autocheck.user_check_in()
-    print(json.loads(res))
+    print(json.loads(res.decode('utf-8')))
     return HttpResponse(res)
